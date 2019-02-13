42917214296188763a84928216afc29a8550e035
Otavio Araujo <otavio-luiz17@outlook.com>
2019-02-13 00:49:56 -0200

    App

---
+++ App/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
